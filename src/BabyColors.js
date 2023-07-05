4b065dce5dcbe39db00a405aa7c3a1a0a01c81cb
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-05 23:15:55 +0100

    done level 3
    create favorite list

--- src/BabyColors.js
+++ src/BabyColors.js
@@ -1,57 +1,79 @@
 import React, { useState } from "react";
 import babyNames from "./BabyNames";
+import FavoritesList from "./FavoritesList";
 
 export default function BabyColors() {
-
-    // sort names
+  // sort names
     const sortedNames = [...babyNames].sort((a, b) => {
         return a.name.localeCompare(b.name);
     });
 
-    // useState for search
+  // useState for search & favorites
     const [text, setText] = useState("");
     const [filteredNames, setFilteredNames] = useState([...sortedNames]);
+    const [favoriteList, setFavoriteList] = useState([]);
 
-    // filter Names 
+  // filter Names
     const filterNames = () => {
         const searchInput = text.toLowerCase();
         const searchForNames = sortedNames.filter((el) =>
-            el.name.toLowerCase().includes(searchInput)
+        el.name.toLowerCase().includes(searchInput)
         );
         setFilteredNames(searchForNames);
-    }
+    };
 
     const handleChange = (event) => {
         setText(event.target.value);
         filterNames();
+        event.preventDefault()
     };
 
+    const handleFavorite = (el) => {
+        setFavoriteList(() => [...favoriteList, {name: el.name, sex: el.sex}]);
+        removeNames(el);
+    };
+
+    const removeNames = (el) => {
+        setFilteredNames(() => {
+            filteredNames.splice(filteredNames.indexOf(el), 1)
+            return filteredNames;
+        })
+    };
+    
+
+
     return (
-    <div >
+        <div>
             <div className="search">
-                <div></div>
-                <input
+            <></>
+            <input
                 type="text"
                 value={text}
                 id="search"
                 onChange={handleChange}
                 placeholder="Search Names..."
-                />
+            />
             </div>
-        <div className="border-box">
-            {filteredNames.map((el) => (
+            <></>
+                <div>
+                <FavoritesList favoriteList={favoriteList} />
+                </div>
+                <div className="border-box">
+                {filteredNames.map((el) => (
             <div
                 style={{
-                backgroundColor:
-                el.sex === "f" ? "rgb(255, 156, 215)" : "rgb(168, 212, 255)",
+                    backgroundColor:
+                    el.sex === "f" ? "rgb(255, 156, 215)" : "rgb(168, 212, 255",
                 }}
                 className="baby-box"
+                value={el.sex}
                 key={el.id}
+                onClick={() => handleFavorite(el)}
                 >
                 {el.name}
             </div>
             ))}
+            </div>
         </div>
-    </div>
     );
 }
