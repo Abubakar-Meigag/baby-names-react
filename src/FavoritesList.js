dceae29c7136f2f18ba1cc3135ea63d20ef480a1
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-08 21:23:35 +0100

    update change some name variable

--- src/FavoritesList.js
+++ src/FavoritesList.js
@@ -8,18 +8,18 @@ const FavoritesList = ({ favoriteList }) => {
         <h2>Favorite Name List</h2>
       </div>
       <div className="favorite-box-item">
-        {favoriteList.map((name, index) => {
+        {favoriteList.map((info, index) => {
           return (
-              <div
-                style={{
-                  backgroundColor:
-                    name.sex === "f" ? "rgb(255, 156, 215)" : "rgb(168, 212, 255",
-                }}
-                className="favoriteList-item"
-                key={index}
-              >
-                {name.name}
-              </div>
+            <div
+              style={{
+                backgroundColor:
+                  info.sex === "f" ? "rgb(255, 156, 215)" : "rgb(168, 212, 255",
+              }}
+              className="favoriteList-item"
+              key={index}
+            >
+              {info.name}
+            </div>
           );
         })}
       </div>
